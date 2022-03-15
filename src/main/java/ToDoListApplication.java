e5258567dab9dc68c9f8c3cc20061810ff2d85e4
Anton Harbuz <4ack90@gmail.com>
2022-03-15 20:14:14 +0300

    setup

---
+++ src/main/java/ToDoListApplication.java
@@ -0,0 +1,9 @@
+public class ToDoListApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, please make a choice:");
+        System.out.println("1. Create todo");
+        System.out.println("2. Get all todo");
+        System.out.println("3. Delete todo");
+    }
+}
